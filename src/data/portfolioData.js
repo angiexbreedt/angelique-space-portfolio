e22175ec4f6b99d32233258b16ce9b4801264bda
Angelique Breedt <u23542838@tuks.co.za>
2025-10-16 16:49:58 +0200

    Add src/data/portfolioData.js

---
+++ src/data/portfolioData.js
@@ -0,0 +1,20 @@
+// This file will be dynamically generated with user data
+const portfolioData = {
+  header: {
+    name: "Angelique Breedt",
+    title: "Professional",
+    summary: "Computer Science Student"
+  },
+  skills: ["Java","Python","React","JavaScript","OpenGL","Github","SQL","R"],
+  experience: [{"title":"Tutor","company":"Target Tutors","startDate":"Jan 2022","endDate":"May 2023","extra":["Tutor for high school and primary school students, subjects include Mathematics, Biology, Afrikaans, Natural Sciences, etc."]},{"title":"Marketing Manager","company":"Breedt Braais","startDate":"Jan 2021","endDate":"present","extra":["Responsible for planning, developing, and executing strategies to promote products and increase sales, create ads and manage the Instagram account"]},{"title":"Pre-school child carer & teaching assistant","company":"Huggy Bears, Bedfordview","startDate":"Mar 2022","endDate":"Nov 2022","extra":["Cared for children aged 2 - 6 years, assisted in developing and refining fine motor and gross motor skills, art, play-time, story-time, and PE"]}],
+  education: [{"institution":"University of Pretoria","degree":"BSc Computer Science","field":"Computer Science","endDate":"2023 - 2025","gpa":""},{"institution":"ACS","degree":"Matric","field":"","endDate":"2009 - 2021","gpa":""}],
+  projects: [{"title":"","description":"Leading development efforts as part of COS301 (Software Engineering) Capstone project","technologies":[],"dateRange":"","url":""}],
+  contact: {
+    email: "",
+    phone: "",
+    linkedin: "",
+    github: ""
+  }
+};
+
+export default portfolioData;
